3bec113393b977a9f65e3be2995824404f084133
Jishnu K <jishnuk7676@gmail.com>
2026-03-09 15:42:33 +0530

    Fix indentation and add new prediction input

--- decisiontree.py
+++ decisiontree.py
@@ -6,5 +6,6 @@ X = [[30],[40],[50],[60],[20],[10],[70]]
 y = [0,1,1,1,0,0,1]
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X,y)
-X_marks=[[20]]
+X_marks=[[30]]
+
 print(classifier.predict(X_marks))

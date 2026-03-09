6b3a78e25e7e755b94bbf83b61809067d3440f78
Jishnu K <jishnuk7676@gmail.com>
2026-03-09 15:44:54 +0530

    Change prediction input from 30 to 20

--- decisiontree.py
+++ decisiontree.py
@@ -6,6 +6,7 @@ X = [[30],[40],[50],[60],[20],[10],[70]]
 y = [0,1,1,1,0,0,1]
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X,y)
-X_marks=[[30]]
+X_marks=[[20]]
 
 print(classifier.predict(X_marks))
+
